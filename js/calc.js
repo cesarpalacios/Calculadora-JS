7363d4d0cd513f7a652d8fc5224bf37712d8977e
ceapalaciosal <ceapalaciosal@gmail.com>
2018-08-29 19:10:22 -0500

    se suma jQuery

---
+++ js/calc.js
@@ -0,0 +1,13 @@
+<script type="text/javascript" href="js/jquery-3.3.1.min.js"></script>
+
+function ejecutar(){
+    jQery()
+    //var variable1 = document.getElementById("nro1").value;
+    var variable1 = jQery("#nro1").val();
+
+    var variable2 = jQery('#nro2').val();
+
+
+}
+
+jQery()
